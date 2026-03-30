47465fd32618450b4794b09c9f4728d13bef8128
Harshitlens <harshitshukla@lensvox.com>
2026-03-30 10:24:52 +0530

    Upate in noification flow

--- backend/apps/notification/views.py
+++ backend/apps/notification/views.py
@@ -10,7 +10,6 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
-
 # Import your custom authentication if needed
 from apps.users.auth import StaticTokenAuthentication
 
